d70a34dc44fad9be7b3b5dd12df4d3af05ac50bf
Omhedau <100467711+Omhedau@users.noreply.github.com>
2024-03-31 19:59:47 +0530

    products page with productCard componet added

---
+++ frontend/src/components/ProductCard.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const ProductCard = () => {
+  return (
+    <div className=' w-full h-[25rem] bg-slate-100 shadow-md hover:scale-105 duration-500'>
+        <div className=' bg-slate-200 w-full h-[13rem]'>
+            <img className='w-full h-full object-cover object-top' src="https://images2.alphacoders.com/133/1338870.png" />
+        </div>
+        <div className='flex justify-between items-center px-5 mt-5'>
+            <p className='text-2xl font-bold'>Tracter</p>
+            <div className=' bg-slate-200'>
+            <i class="ri-heart-line" style={{fontSize: '27px'}}></i>
+            </div>
+        </div>
+        <p className=' text-xs px-5 mt-2 w-[90%]'>
+          Lorem ipsum dolor, sit amet coatui quidem molestiae similique! Provident.
+        </p>
+        <button className='bg-blue-200 mt-5 p-2 mr-2 float-end hover:bg-blue-400'>
+            Book Now
+        </button>
+    </div>
+  )
+}
+
+export default ProductCard;
